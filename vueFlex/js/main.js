766ea389b0a66a356432e5a5e92089b134489cc2
tanbiqin <939547533@qq.com>
2017-06-09 17:14:50 +0800

    add BenzCOC

--- vueFlex/js/main.js
+++ vueFlex/js/main.js
@@ -1,24 +1,26 @@
 var app = new Vue({
   el: '#app',
   data: {
-   checkedFlag:false,
+   checkedFlag:0,
+   gradeOrder:false,
+   timeOrder:false,
    testList:[
 		{
       grades:100,
       title:'关于宠物营养的随堂测试1',
       detail:'课程：我们一直在努力推动宠物营养前进',
-      date:'2017年2月5日'
+      date:'2018年2月5日'
     },{
 			grades:89,
 			title:'关于宠物营养的随堂测试2',
 			detail:'课程：我们一直在努力推动宠物营养前进',
-			date:'2016年2月5日'
+			date:'2015年2月5日'
 		},
 		{
 			grades:70,
 			title:'关于宠物营养的随堂测试3',
 			detail:'课程：如何促进猫星人的健康体重？',
-			date:'2017年5月5日'
+			date:'2016年5月5日'
 		},
 		{
 			grades:0,
@@ -30,18 +32,41 @@ var app = new Vue({
   },
   mounted: function () {
   	this.$nextTick(function(){
-	     //this.selectOrderWay();
+	     this.selectOrderWay();
   	});
   },
   methods: {
   	selectOrderWay:function(checkedFlag){
-      this.checkedFlag = true;
-  		/*if(typeof item.checked == 'undefined'){
-        Vue.set(item, "checked", true); //全局注册
-        //this.$set(item, "checked", true); //局部注册
-      }else{
-          item.checked = !item.checked;
-      }*/
+      this.checkedFlag = checkedFlag;
+  		if (checkedFlag == 1) {
+        var  gradeOrder = this.gradeOrder;
+        this.timeOrder = false;  //去掉时间排序样式
+        this.testList.sort(function(obj1,obj2){
+          if (gradeOrder) {
+            return obj1.grades - obj2.grades;
+          }else{
+            return obj2.grades - obj1.grades;
+          }
+        });
+        this.gradeOrder = !this.gradeOrder;
+      }else if (checkedFlag == 2) {
+        var timeOrder = this.timeOrder;
+        this.gradeOrder = false;  
+        this.testList.sort(function(obj1,obj2){
+          var date1 = ((obj1.date.replace(/年/, "/")).replace(/月/, "/")).replace(/日/, " "); //转换日期格式
+          var date2 = ((obj2.date.replace(/年/, "/")).replace(/月/, "/")).replace(/日/, " ");
+          if (timeOrder) {
+            return date1 < date2 ? 1 : -1; 
+          }else{
+            return date1 > date2 ? 1 : -1; 
+          }
+
+        });
+        this.timeOrder = !this.timeOrder;
+      }else if (checkedFlag === 0) {
+          this.gradeOrder = false;
+          this.timeOrder = false;
+      }
   	}
   }
 });
